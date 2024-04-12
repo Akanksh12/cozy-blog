87711e6d570995bc6c34e5a1471049851ef3f134
Akanksh Chitimalla <55909985+Akanksh12@users.noreply.github.com>
2024-04-12 20:02:29 +0530

    added about and added everything to public folder

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
    res.sendFile(__dirname + "/index.html")
 })
 
+app.get('/', (req, res) => {
+   res.sendFile(__dirname + "/about.html")
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
