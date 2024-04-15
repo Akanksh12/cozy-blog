620bf5ccf2f362aa0721138df8e3a77b4ae3b1f1
Akanksh Chitimalla <55909985+Akanksh12@users.noreply.github.com>
2024-04-15 11:57:43 +0530

    Using showdownjs to convert markdown to html

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('node:path');
+const fs = require('node:fs');
+
+var showdown  = require('showdown'),
+    converter = new showdown.Converter();
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, "public")})
@@ -11,13 +15,17 @@ app.get('/about', (req, res) => {
     res.sendFile('about.html', {root: path.join(__dirname, "public")})
 })
 
-app.get('*', (req, res) => {
-    res.sendFile('404.html', {root: path.join(__dirname, "public")})
+app.get(/\/posts\/.*/, (req, res) => {
+    console.log(path.join(__dirname, req.path + ".md"))
+    fs.readFile(path.join(__dirname, req.path + ".md"), (err, data) => {
+        if (err) {
+           res.redirect(404, '/notfound') 
+        };
+        console.log(data)
+        res.send(converter.makeHtml(data))
+    })
 })
 
-app.get(/\/posts\/.*/, (req, res) => {
-    res.sendFile(req.path + ".md", {root: path.join(__dirname)})
-})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
